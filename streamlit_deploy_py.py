3ac4fa02340250e50d673cebc347ccd313f61cd4
kishanga <85829332+kishanga@users.noreply.github.com>
2024-09-05 10:47:28 +0800

    Update streamlit_deploy_py.py

--- streamlit_deploy_py.py
+++ streamlit_deploy_py.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import pickle
 
+# Load X train columns
+data = pd.read_csv('x-columns.csv')
+
 # Load your trained PyCaret model
 with open('best_model.pkl', 'rb') as f:
     model = pickle.load(f)
